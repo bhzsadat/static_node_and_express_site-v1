6df85271d3b06d21df44ee4c6d71d23ce628544c
Behnaz Sadat <bhz.sadat@gmail.com>
2025-01-03 09:56:01 -0800

    Add 5th project

--- app.js
+++ app.js
@@ -4,31 +4,51 @@ const path = require('path');
 const pug = require('pug');
 const app = express();
 
-
+// set up static route
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
 
+// set up routes
 app.get('/', (req, res) => {
   res.render('index', { projects: data.projects });
 });
 
+// about route
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
+// project route
 app.get('/project/:id', (req, res) => {
-    // const project = data.projects.find(proj => proj.id === req.params.id); // Example lookup
-    // if (project) {
-    //     res.render('project', { project });
-    // } else {
-    //     res.status(404).send('Project not found');
-    // }
+
     const projectId = req.params.id;
     const project = data.projects[req.params.id];
     console.log(project);
     res.render('project', { project });
 });
 
+// 404 handler
+app.use((req, res, next) => {
+  const err = new Error('Page Not Found');
+  err.status = 404;
+  console.error(`Error ${err.status}: ${err.message}`);
+  res.status(404).send(`
+    <h1>Error 404</h1>
+    <p>${err.message}</p>
+  `);
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  err.status = err.status || 500;
+  err.message = err.message || 'Internal Server Error';
+  console.error(`Error ${err.status}: ${err.message}`);
+  res.status(err.status).send(`
+    <h1>Error ${err.status}</h1>
+    <p>${err.message}</p>
+  `);
+});
+
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
-  });
+});
